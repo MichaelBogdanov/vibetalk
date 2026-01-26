eccb75b4c411aa84419ec77647505adfe1b46605
Rick Alderson <rick4lderson@gmail.com>
2026-01-26 23:08:09 +0300

    Изменены маршруты для WebSocket

--- website/routing.py
+++ website/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     # Подключение по паре пользователей: ws://.../ws/dm/<peer_user_id>/
-    re_path(r"ws/dm/(?P<peer_id>\d+)/$", consumers.PrivateChatConsumer.as_asgi()),
+    re_path(r"ws/dm/(?P<peer_id>\d+)/?$", consumers.PrivateChatConsumer.as_asgi()),
 ]
