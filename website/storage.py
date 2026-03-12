e33cb33c765de9a8bcba14a6b1de2903a40d7d13
rick4lderson <61159927+MichaelBogdanov@users.noreply.github.com>
2026-03-12 22:55:11 +0300

    Тест деплоя

--- website/storage.py
+++ website/storage.py
@@ -5,3 +5,4 @@ class PrivateMediaStorage(FileSystemStorage):
     def __init__(self, *args, **kwargs):
         kwargs['location'] = settings.PRIVATE_MEDIA_ROOT
         super().__init__(*args, **kwargs)
+        
